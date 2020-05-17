bed4842ff8efaf0f54646bdfc5eb8824c3a605e8
Plover Brown <rellwoos@gmail.com>
2020-05-16 19:37:26 -0700

    getting the views up and running

--- guide/urls.py
+++ guide/urls.py
@@ -4,6 +4,6 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('', views.plant_list, name='plant_list'),
+    path('plant/<int:pk>/', views.plant_detail, name='plant_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- guide/views.py
+++ guide/views.py
@@ -5,3 +5,8 @@ def plant_detail(request, pk):
     '''view for individual plant pages'''
     plant = get_object_or_404(Plant, pk=pk)
     return render(request, 'guide/plant_detail.html', { 'plant':plant })
+
+def plant_list(request):
+    '''view for main page'''
+    plants = Plant.objects
+    return render(request, "guide/plant_list.html", {'plants':plants})
